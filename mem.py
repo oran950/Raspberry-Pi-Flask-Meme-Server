ab10f17434e9c165cea235908c4bd6aa32b0bcc5
Oran Ben David <43114098+oran950@users.noreply.github.com>
2024-03-18 23:14:56 +0200

    Update mem.py

--- mem.py
+++ mem.py
@@ -10,7 +10,7 @@ def get_meme():
     url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
-    subreddit = response["subreddit"]
+    subreddit = response["subreddit"] 
     return meme_large, subreddit
 
 
